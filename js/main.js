487d6cc9df779b18cb1b4f80275fe6bfa22aff1b
Jakub Kania <kubak.15@gmail.com>
2017-04-05 19:44:43 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.pull').on('click', function(){
+        $('.responsive-menu').slideToggle(200);
+    });
+});
+
+$(document).ready(function(){
+    $('.row-2 .tech-1 ul li').tooltip();
+});
+   
---
+++ potwierdzenie.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Formularz Kontaktowy</title>
+
+<style>
+*
+{
+	margin: 0;
+	padding: 0;
+    box-sizing: border-box;
+}
+body
+    {
+        background: #252529;
+        font-family: 'Lato', sans-serif;
+        color: #fff;
+    }
+    #formularz p
+    {
+        font-size: 44px;
+    }
+    .container
+    {
+        width: 700px;
+        height: 500px;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+        margin-top: 150px;
+        text-align: center;
+    }
+    
+</style>
+
+</head>
+
+<body>
+    
+    <div class="container">
+        <div id="formularz">
+        <p>Twoja wiadomość została wysłana.</p>
+        </div>
+    </div>
+</body>
+
+</html>
